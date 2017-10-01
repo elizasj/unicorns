2909c1cb3df31fbdfeea5e9e678c4b688b141311
elizasj <eliza.struthersjobin@gmail.com>
2017-10-01 20:14:27 +0200

    fix mobile menu fix js across pages

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-console.log("🦊 Hello! Edit me in src/js/app.js");
+console.log('🦊 Hello! Edit me in src/js/app.js');
 
 import { uniq } from 'lodash';
-import p5 from 'p5'
+import p5 from 'p5';
 
-import dropDown from './js/test';
+import toggleMenu from './js/test';
 
-const ages = [ 1, 1, 4, 52, 12, 4]
+// const ages = [1, 1, 4, 52, 12, 4];
 
-console.log(uniq(ages));
+// console.log(uniq(ages));
 
-const myp5 = new p5(function (sketch) {
-    sketch.setup = function() {
-        console.log("inside setup")
-    }
+// const myp5 = new p5(function(sketch) {
+//   sketch.setup = function() {
+//     console.log('inside setup');
+//   };
 
-    sketch.draw = function() {}
-})
-
+//   sketch.draw = function() {};
+// });
---
+++ src/js/test.js
@@ -0,0 +1,24 @@
+function resizeWindow() {
+  if (matchMedia) {
+    const mq = window.matchMedia('(min-width: 60em)');
+  
+    mq.addListener(toggleMenu);
+    toggleMenu(mq);
+  }
+}
+
+function toggleMenu(mq) {
+  const menu = document.getElementById('menu')
+  // window is > 60em
+  if (mq.matches) {    
+    menu.classList.remove('overlay', 'overlay__panel')
+    
+    // window is > 60em
+  } else {
+    menu.classList.add('overlay', 'overlay__panel')
+  }
+}
+
+resizeWindow()
+
+export default toggleMenu();
